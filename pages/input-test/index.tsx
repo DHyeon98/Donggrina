f86965829207e9cdb10919c219c706eb1d2e874b
LimDong <ilhai6@naver.com>
2024-06-02 23:51:25 +0900

    빌드 테스트

--- pages/input-test/index.tsx
+++ pages/input-test/index.tsx
@@ -1,22 +1,22 @@
-// import React from 'react';
-// import { useForm, FormProvider } from 'react-hook-form';
-// import FormInput from '@/components/common/Input';
+import React from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
+import FormInput from '@/components/common/Input';
 
-// function App() {
-//   const methods = useForm();
+function App() {
+  const methods = useForm();
 
-//   const onSubmit = (data: any) => {
-//     console.log(data);
-//   };
+  const onSubmit = () => {
+    console.log('a');
+  };
 
-//   return (
-//     <FormProvider {...methods}>
-//       <form onSubmit={methods.handleSubmit(onSubmit)}>
-//         <FormInput name="name" label="가족의 이름을 알려주세요!" />
-//         <FormInput name="nickname" label="가족 내 본인의 이름을 입력해주세요." />
-//       </form>
-//     </FormProvider>
-//   );
-// }
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <FormInput name="name" label="가족의 이름을 알려주세요!" />
+        <FormInput name="nickname" label="가족 내 본인의 이름을 입력해주세요." />
+      </form>
+    </FormProvider>
+  );
+}
 
-// export default App;
+export default App;

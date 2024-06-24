a50c4f668666d124a9c530d0a98548a6f189769d
LimDong <ilhai6@naver.com>
2024-06-24 16:06:47 +0900

    미들웨어 변경

--- pages/family/index.tsx
+++ pages/family/index.tsx
@@ -3,8 +3,10 @@ import styles from './index.module.scss';
 import FamilyDiary from '@/components/family/diary/family-diary';
 import FamilyGrowth from '@/components/family/growth/family-growth';
 import FamilySchedule from '@/components/family/schedule/family-schedule';
+import { getCookie } from 'cookies-next';
 
 export default function Family() {
+  console.log(getCookie('AccessToken'));
   return (
     <section className={styles['family-section']}>
       <div className={styles.today}>{CalendarInstance.getToday()}</div>

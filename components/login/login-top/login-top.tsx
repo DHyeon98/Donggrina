c1cafd93311555973f37defe1e0f2b7b1588e597
LimDong <ilhai6@naver.com>
2024-06-25 02:56:13 +0900

    배포 테스트

--- components/login/login-top/login-top.tsx
+++ components/login/login-top/login-top.tsx
@@ -7,7 +7,9 @@ export default function LoginTop() {
       <dt className={styles.imgBox}>
         <Image src="/images/login/logo.png" alt="로고" layout="fill" />
       </dt>
-      <dd className={styles.text}>동그리나와 관련된 소개 문구</dd>
+      <dd className={styles.text}>
+        동그리나에서 <b>가족, 친구, 연인</b>과 함께 반려동물의 일상을 공유해보세요.
+      </dd>
     </dl>
   );
 }

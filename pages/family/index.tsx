9a7665f216a50993ffde829481299f3ba07db943
LimDong <ilhai6@naver.com>
2024-06-24 16:10:05 +0900

    미들웨어 변경

--- pages/family/index.tsx
+++ pages/family/index.tsx
@@ -3,13 +3,18 @@ import styles from './index.module.scss';
 import FamilyDiary from '@/components/family/diary/family-diary';
 import FamilyGrowth from '@/components/family/growth/family-growth';
 import FamilySchedule from '@/components/family/schedule/family-schedule';
-import { getCookie } from 'cookies-next';
+import { deleteCookie, getCookie } from 'cookies-next';
 
 export default function Family() {
   console.log(getCookie('AccessToken'));
+  const handleDelete = () => {
+    deleteCookie('AccessToken');
+    console.log('a');
+  };
   return (
     <section className={styles['family-section']}>
       <div className={styles.today}>{CalendarInstance.getToday()}</div>
+      <button onClick={handleDelete}>버튼</button>
       <FamilyDiary />
       <FamilyGrowth />
       <FamilySchedule />
